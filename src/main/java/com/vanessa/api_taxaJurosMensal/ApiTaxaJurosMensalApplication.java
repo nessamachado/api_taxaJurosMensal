a297fd90f83ac4f769243d13d1d30e3db9b800ec
Vanessa Machado <van.a.s@hotmail.com>
2023-04-05 14:19:53 -0300

    criação do projeto

---
+++ src/main/java/com/vanessa/api_taxaJurosMensal/ApiTaxaJurosMensalApplication.java
@@ -0,0 +1,13 @@
+package com.vanessa.api_taxaJurosMensal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTaxaJurosMensalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTaxaJurosMensalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vanessa/api_taxaJurosMensal/ApiTaxaJurosMensalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanessa.api_taxaJurosMensal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTaxaJurosMensalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
