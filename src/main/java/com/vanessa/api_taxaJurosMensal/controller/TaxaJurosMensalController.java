f947e8b4a398dbe582a46bb33a11b2b403d7bc6e
Vanessa Machado <van.a.s@hotmail.com>
2023-04-11 14:31:38 -0300

    Criacao do método buscarTodos(findAll)

--- src/main/java/com/vanessa/api_taxaJurosMensal/controller/TaxaJurosMensalController.java
+++ src/main/java/com/vanessa/api_taxaJurosMensal/controller/TaxaJurosMensalController.java
@@ -1,4 +1,31 @@
 package com.vanessa.api_taxaJurosMensal.controller;
 
+import com.vanessa.api_taxaJurosMensal.model.mapper.TaxaJurosMensalMapper;
+import com.vanessa.api_taxaJurosMensal.model.response.TaxaJurosMensalResponse;
+import com.vanessa.api_taxaJurosMensal.service.TaxaJurosMensalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/TaxaJuros")
 public class TaxaJurosMensalController {
+
+    @Autowired
+    private TaxaJurosMensalService taxaJurosMensalService;
+
+    @Autowired
+    private TaxaJurosMensalMapper taxaJurosMensalMapper;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<TaxaJurosMensalResponse> buscarTodasTaxas(){
+        return taxaJurosMensalService.encontrarTodos();
+    }
+
 }
--- src/main/java/com/vanessa/api_taxaJurosMensal/service/TaxaJurosMensalService.java
+++ src/main/java/com/vanessa/api_taxaJurosMensal/service/TaxaJurosMensalService.java
@@ -1,4 +1,28 @@
 package com.vanessa.api_taxaJurosMensal.service;
 
+import com.vanessa.api_taxaJurosMensal.model.dto.TaxaJurosMensalDTO;
+import com.vanessa.api_taxaJurosMensal.model.entities.TaxaJurosMensalEntity;
+import com.vanessa.api_taxaJurosMensal.model.mapper.TaxaJurosMensalMapper;
+import com.vanessa.api_taxaJurosMensal.model.response.TaxaJurosMensalResponse;
+import com.vanessa.api_taxaJurosMensal.repositorie.TaxaJurosMensalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TaxaJurosMensalService {
+
+    @Autowired
+    private TaxaJurosMensalRepository rep;
+
+    @Autowired
+    private TaxaJurosMensalMapper taxaJurosMensalMapper;
+
+    public List<TaxaJurosMensalResponse> encontrarTodos() {
+        List<TaxaJurosMensalEntity> taxaJurosMensalEntityList = rep.findAll();
+        List<TaxaJurosMensalDTO> taxaJurosMensalDTOList = taxaJurosMensalMapper.createEntityToDtoList(taxaJurosMensalEntityList);
+        List<TaxaJurosMensalResponse> taxaJurosMensalResponseList =taxaJurosMensalMapper.createDtoToResponseList(taxaJurosMensalDTOList);
+        return taxaJurosMensalResponseList;
+    }
 }
